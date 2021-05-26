23a551e7892611833e103943de83b7ba9d607375
nflam <nadja.flam@gmx.net>
2021-05-26 12:10:09 +0800

    connection DB

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    port: 3306,
+  
+    user: 'root',
+  
+    password: 'Perth2013',
+    database: 'employees_db',
+  });
+
+  connection.connect();
+
+  connection.query = util.promisify(connection.query);
+
+  module.exports = connection;
